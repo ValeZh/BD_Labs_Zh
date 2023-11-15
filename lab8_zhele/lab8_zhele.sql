8f7258ab187c4cdfa848a658bb8b94af57ff8c71
ValeZh <mugikaoven@gmil.com>
2023-11-15 08:38:18 +0200

    lab8 changed

--- lab8_zhele/lab8_zhele.sql
+++ lab8_zhele/lab8_zhele.sql
@@ -26,7 +26,6 @@ CREATE TABLE `file_project` (
   KEY `project_id` (`project_id`),
   CONSTRAINT `file_project_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`),
   CONSTRAINT `file_project_ibfk_2` FOREIGN KEY (`file_id`) REFERENCES `files` (`id`),
-  CONSTRAINT `file_project_ibfk_3` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
@@ -73,7 +72,6 @@ CREATE TABLE `tasks` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   KEY `project_id` (`project_id`),
-  CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   CONSTRAINT `tasks_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   CONSTRAINT `tasks_ibfk_3` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
